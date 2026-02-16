5a373e4a50a2c02723ba2de53e364e549454f6b5
AlexGB12 <cguitz-2022434@kinal.edu.gt>
2026-02-15 18:35:54 -0600

    modulo de inventory creado

--- configs/app.js
+++ configs/app.js
@@ -12,7 +12,7 @@ import restaurantsRoutes from "../src/restaurants/restaurants-routes.js";
 import gastronomicEventsRoutes from "../src/gastronomicEvents/gastronomicEvents-routes.js";
 import tablesRoutes from "../src/tables/tables-routes.js";
 import productsRoutes from "../src/products/products-routes.js";
-
+import suppliesInventoryRoutes from "../src/suppliesInventory/suppliesInventory-routes.js";
 const BASE_URL = '/bite-and-go/v1';
 
 // Rutas
@@ -34,6 +34,7 @@ const routes = (app) => {
     app.use(`${BASE_URL}/gastronomicEvents`, gastronomicEventsRoutes);
     app.use(`${BASE_URL}/tables`, tablesRoutes);
     app.use(`${BASE_URL}/products`, productsRoutes);
+    app.use(`${BASE_URL}/suppliesInventory`, suppliesInventoryRoutes);
 }
 
 // funcion para iniciar el servidor
